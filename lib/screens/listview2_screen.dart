bd2eedfdbd44942e87b51dec4bc913546d78d9de
arielbakal <arielbakal.dev@gmail.com>
2022-05-31 11:03:45 -0300

    init

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  final options = const ['Minecraft', 'Biosock', 'Osu!', 'League of Legends'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Listview Tipo 1')),
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, i) => ListTile(
+            title: Text(options[i]),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            onTap: () {
+              final game = options[i];
+              print( game );
+            },
+          ),
+          separatorBuilder: ( _ , __ ) => const Divider(),
+        ));
+  }
+}
