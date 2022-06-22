991150f72af0d52a2f269c7cb962fdd925cbb0ac
arielbakal <arielbakal.dev@gmail.com>
2022-06-22 11:52:56 -0300

    93

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -8,27 +8,76 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Ariel',
+      'last_name' : 'Bakal',
+      'email'     : 'arielbakal.dev@gmail.com',
+      'password'  : '12345',
+      'role'      : 'Admin'
+    };
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric( horizontal: 20, vertical: 10),
-          child: Column(
-            children: const [
-              CustomInputField( labelText: 'Nombre', hintText: 'Nombre del usuario'),
-              SizedBox( height: 30 ),
+          padding: const EdgeInsets.symmetric( horizontal: 20, vertical: 15),
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                
+                CustomInputField( labelText: 'Nombre', hintText: 'Nombre del usuario', formProperty: 'first_name', formValues: formValues,),
+                const SizedBox( height: 30 ),
+          
+                CustomInputField( labelText: 'Apellido', hintText: 'Apellido del usuario', formProperty: 'last_name', formValues: formValues,),
+                const SizedBox( height: 30 ),
+          
+                CustomInputField( labelText: 'Email', hintText: 'Email del usuario', keyboardType: TextInputType.emailAddress, formProperty: 'email', formValues: formValues,),
+                const SizedBox( height: 30 ),
+                
+                CustomInputField( labelText: 'Contraseña', hintText: 'Contraseña del usuario', obscureText: true, formProperty: 'password', formValues: formValues,),
+                const SizedBox( height: 30 ),
 
-              CustomInputField( labelText: 'Apellido', hintText: 'Apellido del usuario'),
-              SizedBox( height: 30 ),
+                DropdownButtonFormField<String>(
+                  value: 'Admin',
+                  items: const [ 
+                    DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                    DropdownMenuItem(value: 'Superuser', child: Text('Superuser')),
+                    DropdownMenuItem(value: 'Developer', child: Text('Developer')),
+                    DropdownMenuItem(value: 'Jr. Developer', child: Text('Jr. Developer')),
+                  ], 
+                  onChanged: ( value ) {
+                    print(value);
+                    formValues['role'] = value ?? 'Admin';
+                  }
+                ),
+                const SizedBox( height: 20 ),
 
-              CustomInputField( labelText: 'Email', hintText: 'Email del usuario', keyboardType: TextInputType.emailAddress),
-              SizedBox( height: 30 ),
-              
-              CustomInputField( labelText: 'Contraseña', hintText: 'Contraseña del usuario', obscureText: true),
-              SizedBox( height: 30 ),
-            ],
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: Text('Guardar'))
+                  ),
+                  onPressed: () {
+
+                    FocusScope.of(context).requestFocus( FocusNode() );
+                    
+                    if ( !myFormKey.currentState!.validate() ) {
+                      print('Formulario no valido');
+                      return;
+                    }
+
+                    print(formValues);
+                  },
+                ),
+              ],
+            ),
           ), 
         ),
       ),
